3a4915ea1f47b87ed57063240b025da21e9bdaac
Anish G <anishgoyal1108@gmail.com>
2022-11-16 00:00:17 -0500

    added solutions

---
+++ grains-of-sand/grains-of-sand.cpp
@@ -0,0 +1,163 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<set>
+#include<cstring>
+#include<string>
+#include<numeric>
+#include<sstream>
+#include<iterator>
+#include<bitset>
+#include<climits>
+
+using namespace std;
+typedef long long int lli;
+
+#define all(arr) arr.begin(),arr.end()
+#define f first
+#define s second
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<" "<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n"
+#define nl cout<<"\n";
+#define pq priority_queue
+#define inf 0x3f3f3f3f
+#define test cout<<"abcd\n";
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define pb push_back
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define space ' '
+#define kick(t) cout << "Case #" << t+1 << ":" << endl;
+
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+template <typename T>
+void input(vector<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.push_back(temp);
+}
+
+template <typename T>
+void output(vector<T> arr) {
+  for(auto x:arr) cout<<x<<" ";
+  cout<<endl;
+}
+
+
+template <typename T>
+void input_set(set<T> &arr,lli n) {
+  T temp;
+  for(lli i=0;i<n;i++) cin>>temp, arr.insert(temp);
+}
+
+lli mul(lli a, lli b) {
+  return (a%MOD*b%MOD)%MOD;
+}
+
+lli power(lli a,lli b) {
+  lli ans = 1;
+  while(b > 0) {
+    if(b&1)
+      ans = mul(ans, a);
+    a = mul(a,a);;
+    b >>= 1;
+  }
+  return ans;
+}
+
+void split(const string &s, char delim, vector<string> &elems) {
+  stringstream ss;
+  ss.str(s);
+  string item;
+  while (getline(ss, item, delim)) {
+    elems.push_back(item);
+  }
+}
+
+vector<string> split(const string &s, char delim) {
+  vector<string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
+bool toBool(std::string const& s) {
+     return s != "0";
+}
+string addBigNumbers(string number1, string number2) {
+   if (number1.length() > number2.length())
+    swap(number1, number2);
+   string sum = "";
+   int len1 = number1.length();
+   int len2 = number2.length();
+   int digitDiff = len2 - len1;
+   int carry = 0;
+   int intSum;
+   for (int i=len1-1; i>=0; i--) {
+      intSum = ((number1[i]-'0') + (number2[i+digitDiff]- '0') + carry);
+      sum.push_back(intSum%10 + '0');
+      carry = intSum/10;
+   }
+   for (int i=digitDiff-1; i>=0; i--) {
+      intSum = ((number2[i]-'0')+carry);
+      sum.push_back(intSum%10 + '0');
+      carry = intSum/10;
+   }
+   if (carry)
+    sum.push_back(carry+'0');
+   reverse(sum.begin(), sum.end());
+   return sum;
+}
+
+void solve(int testcase) {
+  long long unsigned int n;
+  cin >> n;
+  vector<string> arr = {};
+  for (long long unsigned int i = 0; i < n; i++) {
+      string str;
+      cin >> str;
+      arr.push_back(str);
+  }
+      string result = "0";
+      for (long long unsigned int i = 0; i < arr.size(); i++)
+          result = addBigNumbers(result, arr[i]);
+      cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  lli testcases;
+  cin>>testcases;
+  for(int testcase=0; testcase<testcases; testcase++) {
+    solve(testcase);
+  }
+}
+
