95ed311a520d7f6c13d46929ebcb57609aba2142
Yubo_Cao <cao2006721@gmail.com>
2022-11-22 12:42:33 -0500

    feat: use cin/cout & dynamic sized arrays

--- basic/basic/src/number_range.cpp
+++ basic/basic/src/number_range.cpp
@@ -4,44 +4,36 @@ using namespace std;
 
 const int N = 1e6 + 10;
 
-int n, m;
-int q[N];
+int main() {
+    int n, q;
+    cin >> n >> q;
 
-int main()
-{
-    scanf("%d%d", &n, &m);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &q[i]);
-    while (m--)
-    {
-        int x;
-        scanf("%d", &x);
-        // find start
-        int l = 0, r = n - 1;
-        while (l < r)
-        {
-            int mid = l + r >> 1;
-            if (q[mid] >= x)
-                r = mid;
+    int a[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    while (q-- > 0) {
+        int l = 0, h = n - 1, k;
+        cin >> k;
+        while (l < h) {
+            int mid = l + h >> 1;
+            if (a[mid] >= k)
+                h = mid;
             else
                 l = mid + 1;
         }
-        if (q[l] != x)
-        {
+        if (a[l] != k) {
             cout << "-1 -1" << endl;
             continue;
-        }
-        cout << l << " ";
-        l = 0, r = n - 1;
-        while (l < r)
-        {
-            int mid = l + r + 1 >> 1;
-            if (q[mid] <= x)
+        } else
+            cout << l << " ";
+        l = 0, h = n - 1;
+        while (l < h) {
+            int mid = l + h + 1 >> 1;
+            if (a[mid] <= k)
                 l = mid;
             else
-                r = mid - 1;
+                h = mid - 1;
         }
         cout << l << endl;
     }
-    return 0;
 }
