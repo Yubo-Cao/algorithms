02625b7b1e9a35a43a0d5f9e50468bd79528f130
Anish G <anishgoyal1108@gmail.com>
2023-03-28 23:48:54 -0400

    feat: add batter_up

---
+++ codequest/2020-2021/python/batter_up/main.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    name, rest = input().split(":")
+    bats = [bat for bat in rest.split(",") if bat != "BB"]
+    sum = 0
+    for j in range(len(bats)):
+        sum += (int(bats[j][0]) if (bats[j][0] in "123") else 0)
+        sum += (4 if (bats[j][0] == "H") else 0)
+    print(name + "=" + "%.3f" % round(sum/len(bats), 3))
+        
+            
