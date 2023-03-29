d6f414cb4c98bf10645b52302fadec74d12b6caf
Anish G <anishgoyal1108@gmail.com>
2023-03-28 23:52:59 -0400

    feat: add batter_up

--- codequest/2020-2021/python/batter_up/main.py
+++ codequest/2020-2021/python/batter_up/main.py
@@ -6,6 +6,7 @@ for i in range(n):
     for j in range(len(bats)):
         sum += (int(bats[j][0]) if (bats[j][0] in "123") else 0)
         sum += (4 if (bats[j][0] == "H") else 0)
-    print(name + "=" + "%.3f" % round(sum/len(bats), 3))
+    result = f'{round(sum/len(bats), 3):.3f}'
+    print(name + "=" + result)
         
             
