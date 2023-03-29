8f3cb3f8a8bc94ec967d11240ef5211b46281448
Anish G <anishgoyal1108@gmail.com>
2023-03-29 08:09:41 -0400

    feat: add alternative script

---
+++ codequest/scripts/questPy.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import sys
+
+# Define the base path where the folder should be created
+base_path = f"C:/Users/{os.getlogin()}/algorithms/codequest/2020-2021/python/"
+
+# Get the name of the folder from the first command line argument
+folder_name = sys.argv[1]
+
+# Create the folder if it doesn't exist already
+folder_path = os.path.join(base_path, folder_name)
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+# Create the input and output files inside the folder
+for suffix in ['in', 'out']:
+    file_path = os.path.join(folder_path, f"1.{suffix}")
+    open(file_path, 'a').close()  # create an empty file
+
+# Create the main.py file
+main_file_path = os.path.join(folder_path, "main.py")
+open(main_file_path, 'a').close()  # create an empty file
+
+# Open all three files in a new instance of vscode
+code_path = "C:/Users/Anish/AppData/Local/Programs/Microsoft VS Code/bin/code.cmd"
+subprocess.run([code_path, file_path])
